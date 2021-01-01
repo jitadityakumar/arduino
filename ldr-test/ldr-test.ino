5021e7736bba186aa0c406b4115cb29ed84e0775
Jitaditya Kumar <jitadityakumar@users.noreply.github.com>
2021-01-01 18:04:22 +0000

    LDR test
    LDR test for Arduino Uno

---
+++ ldr-test/ldr-test.ino
@@ -0,0 +1,16 @@
+// LDR Test
+
+int LDR_PIN = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Setup Complete");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("LDR Read");
+  Serial.println(analogRead(LDR_PIN));
+  delay(2000);
+}
